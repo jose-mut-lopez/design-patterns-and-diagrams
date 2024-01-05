575299db7d3d2f69147e51786b88f637e92053a5
jose miguel mut lopez <jose.mut@intrasonics.com>
2024-01-05 16:52:04 +0100

    explain problem-2

--- app/src/test/kotlin/refactoring/AppStrategyTest.kt
+++ app/src/test/kotlin/refactoring/AppStrategyTest.kt
@@ -4,9 +4,11 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class AppStrategyTest {
+
+    // FIXME: app.requestData() tries to read from stdin in a test
     @Test fun getInfo() {
         val app = AppStrategy()
-        assertEquals(app.requestData(), "Hello null from null!")
+        assertEquals(app.requestData(), "Hello someone from somewhere!")
     }
 
 }
