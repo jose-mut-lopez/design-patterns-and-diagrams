318c4a466c84ffa742bb44d095df934ac04e04f1
jose miguel mut lopez <jose.mut@intrasonics.com>
2024-01-14 19:11:15 +0100

    solve InteractiveGreeting: solution 1 with PrintStream and BufferedReader

--- app/src/main/kotlin/problemExternalResource/InteractiveGreeting.kt
+++ app/src/main/kotlin/problemExternalResource/InteractiveGreeting.kt
@@ -1,23 +1,32 @@
 package problemExternalResource
 
-class PersonInfo {
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintStream
+
+class PersonInfo(
+    private val input: BufferedReader,
+    private val output: PrintStream,
+) {
     var name: String? = null
     var location: String? = null
 
     fun requestName() {
-        println("What is your name?")
-        name = readln()
+        output.println("What is your name?")
+        name = input.readLine()
     }
     fun requestLocation() {
-        println("What is your location?")
-        location = readln()
+        output.println("What is your location?")
+        location = input.readLine()
     }
 }
 
 class InteractiveGreeting(
+    input: BufferedReader = BufferedReader(InputStreamReader(System.`in`)),
+    output: PrintStream = System.out,
+    private val personInfo: PersonInfo = PersonInfo(input, output),
 ) {
     fun makeGreeting(): String {
-        val personInfo = PersonInfo()
         personInfo.requestName()
         personInfo.requestLocation()
         return "Hello ${personInfo.name} from ${personInfo.location}!"
--- app/src/test/kotlin/problemExternalResource/InteractiveGreetingTest.kt
+++ app/src/test/kotlin/problemExternalResource/InteractiveGreetingTest.kt
@@ -1,12 +1,15 @@
 package problemExternalResource
 
+import java.io.BufferedReader
+import java.io.StringReader
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class InteractiveGreetingTest {
 
     @Test fun makeGreeting() {
-        assertEquals("Hello someone from somewhere!", InteractiveGreeting().makeGreeting())
+        val greeting = InteractiveGreeting(BufferedReader(StringReader("someone\nsomewhere\n")), System.out)
+        assertEquals("Hello someone from somewhere!", greeting.makeGreeting())
     }
 
 }
