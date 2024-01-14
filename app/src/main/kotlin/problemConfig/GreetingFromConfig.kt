5c16b2682d279c7159b36694cbc6e6a7fd08f93a
jose miguel mut lopez <jose.mut@intrasonics.com>
2024-01-14 18:49:53 +0100

    solve GreetingFromConfig

--- app/src/main/kotlin/problemConfig/GreetingFromConfig.kt
+++ app/src/main/kotlin/problemConfig/GreetingFromConfig.kt
@@ -1,18 +1,14 @@
 package problemConfig
 
-enum class ConfigMan {
-    INSTANCE;
-
-    companion object {
-        var someConfig: Int = 3
-    }
+class ConfigMan {
+    var someConfig: Int = 3
 }
-class GreetingFromConfig {
+class GreetingFromConfig(private val config: ConfigMan) {
     fun makeGreeting(): String {
-        return "Hello World with config ${ConfigMan.someConfig}!"
+        return "Hello World with config ${config.someConfig}!"
     }
 }
 
 fun run() {
-    println(GreetingFromConfig().makeGreeting())
+    println(GreetingFromConfig(ConfigMan()).makeGreeting())
 }
--- app/src/test/kotlin/problemConfig/GreetingFromConfigTest.kt
+++ app/src/test/kotlin/problemConfig/GreetingFromConfigTest.kt
@@ -7,18 +7,20 @@ class GreetingFromConfigTest {
 
     // FIXME: This test fails when running tests in parallel because the ConfigMan is a singleton
     @Test fun defaultConfig() {
-        assertEquals(3, ConfigMan.someConfig)
+        assertEquals(3, ConfigMan().someConfig)
     }
 
     // FIXME: This test fails when running tests in parallel because the ConfigMan is a singleton
+
     @Test fun makeGreeting() {
-        assertEquals("Hello World with config 3!", GreetingFromConfig().makeGreeting())
+        assertEquals("Hello World with config 3!", GreetingFromConfig(ConfigMan()).makeGreeting())
     }
 
     @Test fun updateGreeting() {
-        val greeting = GreetingFromConfig()
+        val config = ConfigMan()
+        val greeting = GreetingFromConfig(config)
         assertEquals("Hello World with config 3!", greeting.makeGreeting())
-        ConfigMan.someConfig = 7
+        config.someConfig = 7
         assertEquals("Hello World with config 7!", greeting.makeGreeting())
     }
 }
